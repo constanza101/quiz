51880ffbb3bde521fd5f15e83880ab67d9a68784
constanza diaz <cdiaz@admira.com>
2021-03-17 00:07:22 +0100

    master: form validation

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="#000"
       dark
     >
       <div class="d-flex align-center">
@@ -15,25 +15,18 @@
           width="40"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+
       </div>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        href="https://github.com/constanza101/quiz"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-icon>mdi-github</v-icon>
+        <span class="mr-2"></span>
       </v-btn>
     </v-app-bar>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,22 +14,37 @@
         <h1 class="display-2 font-weight-bold mb-3">Welcome to Quizful</h1>
       </v-col>
     </v-row>
-    <v-row class="d-flex justify-center">
-      <v-col class="d-flex justify-center" cols="10" md="4">
-        <v-text-field
-          label="Please write down your name here."
-          placeholder="Eg. Constanza"
-          filled
-        ></v-text-field>
-      </v-col>
-    </v-row>
+    <v-form ref="form" v-model="valid">
+      <v-row class="d-flex justify-center">
+        <v-col class="d-flex justify-center" cols="10" md="4">
+          <v-text-field
+            label="Please enter your name here."
+            placeholder="Eg. Constanza"
+            filled
+            :rules="[rules.textRequired]"
+          ></v-text-field>
+        </v-col>
+      </v-row>
 
-    <v-row class="d-flex justify-center">
-      <v-col class="d-flex justify-center" cols="10" md="4">
-        <v-select  v-model="selectedQuiz" :items="quizes" item-text="title"
-            item-value="id" filled label="Choose a quiz"   ></v-select>
-      </v-col>
-    </v-row>
+      <v-row class="d-flex justify-center">
+        <v-col class="d-flex justify-center" cols="10" md="4">
+          <v-select
+            v-model="selectedQuiz"
+            :items="quizes"
+            item-text="title"
+            item-value="id"
+            filled
+            label="Choose a quiz"
+            :rules="[rules.idRequired]"
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-row class="d-flex justify-center pt-8">
+        <v-btn v-on:click="submit" color="#03bbb6" dark depressed large>
+          let's do the quiz!
+        </v-btn>
+      </v-row>
+    </v-form>
   </div>
 </template>
 
@@ -47,14 +62,28 @@ export default {
     return {
       // quizes: Array,
       selectedQuiz: null,
-      quizes: [], 
+      quizes: [],
+      valid: false,
+      rules: {
+        idRequired: (value) => !isNaN(parseFloat(value)) || "Requerido",
+        textRequired: (value) => !!value || "Requerido",
+      },
     };
   },
   created() {
     this.getQuizes();
   },
   methods: {
-   
+    submit(){
+      if (this.$refs.form.validate()) {
+        console.log("OK")
+      } else {
+                console.log("NOT OK")
+
+      }
+
+
+    },
     //Axios:
     async getQuizes() {
       try {
@@ -62,7 +91,7 @@ export default {
           `https://printful.com/test-quiz.php?action=quizzes`
         );
         let result = response.data;
-        console.log(result)
+        console.log(result);
         this.quizes = result;
       } catch (err) {
         console.log(err);
