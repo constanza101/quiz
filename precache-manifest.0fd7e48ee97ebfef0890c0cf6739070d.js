e7f579287f9eb5fde469a30fd6b8954957cb9b57
constanza diaz <cdiaz@admira.com>
2021-03-22 18:48:40 +0100

    deploy

---
+++ precache-manifest.0fd7e48ee97ebfef0890c0cf6739070d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95840a682766f18909cc",
+    "url": "/quiz/css/chunk-vendors.fb1066dc.css"
+  },
+  {
+    "revision": "986eb0a44720cce561188ded726bfcbd",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "8e08545ac78a47a0c00e",
+    "url": "/quiz/js/app.ead48862.js"
+  },
+  {
+    "revision": "95840a682766f18909cc",
+    "url": "/quiz/js/chunk-vendors.7f0918ab.js"
+  },
+  {
+    "revision": "3fdef390786d44aef1f5c613d4cc99d5",
+    "url": "/quiz/manifest.json"
+  },
+  {
+    "revision": "d83bf8436dc9ba24962983f84af7b6ba",
+    "url": "/quiz/printful-logo.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/quiz/robots.txt"
+  }
+]);
