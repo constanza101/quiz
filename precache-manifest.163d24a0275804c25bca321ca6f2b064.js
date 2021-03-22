f90ce1130699c731f6892254d3a11a57f748f6f3
constanza diaz <cdiaz@admira.com>
2021-03-22 17:01:13 +0100

    gh-pages commit

---
+++ precache-manifest.163d24a0275804c25bca321ca6f2b064.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95840a682766f18909cc",
+    "url": "/quiz/css/chunk-vendors.fb1066dc.css"
+  },
+  {
+    "revision": "166e57c019054c54b8ceaebc1f47812a",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "91bca3579aa61b907962",
+    "url": "/quiz/js/app.7a32b2e7.js"
+  },
+  {
+    "revision": "95840a682766f18909cc",
+    "url": "/quiz/js/chunk-vendors.7f0918ab.js"
+  },
+  {
+    "revision": "3fdef390786d44aef1f5c613d4cc99d5",
+    "url": "/quiz/manifest.json"
+  },
+  {
+    "revision": "d83bf8436dc9ba24962983f84af7b6ba",
+    "url": "/quiz/printful-logo.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/quiz/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/quiz/precache-manifest.163d24a0275804c25bca321ca6f2b064.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "quiz-app"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
